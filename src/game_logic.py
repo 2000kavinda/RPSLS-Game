81cb1e84071b63a6b95242a50806d394959b2513
DSSathsarani <210691968+DSSathsarani@users.noreply.github.com>
2025-05-09 02:01:29 +0530

    missing files added & recovered

---
+++ src/game_logic.py
@@ -0,0 +1,22 @@
+import random
+
+choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
+
+def generate_system_choice():
+    return random.choice(choices)
+
+def determine_winner(user_choice, system_choice):
+    if user_choice == system_choice:
+        return "It's a Tie!"
+    
+    winning_cases = {
+        "Rock": ["Scissors", "Lizard"],
+        "Paper": ["Rock", "Spock"],
+        "Scissors": ["Paper", "Lizard"],
+        "Lizard": ["Spock", "Paper"],
+        "Spock": ["Scissors", "Rock"]
+    }
+
+    if system_choice in winning_cases[user_choice]:
+        return "Right Side Win!"
+    return "Right Side Lose!"
---
+++ src/image_loader.py
@@ -0,0 +1,16 @@
+import cv2
+
+def load_images():
+    rock = cv2.imread('images/rock.png')
+    paper = cv2.imread('images/paper.png')
+    scissors = cv2.imread('images/scissors.png')
+    lizard = cv2.imread('images/lizard.png')
+    spock = cv2.imread('images/spock.png')
+
+    rock = cv2.resize(rock, (200, 200))
+    paper = cv2.resize(paper, (200, 200))
+    scissors = cv2.resize(scissors, (200, 200))
+    lizard = cv2.resize(lizard, (200, 200))
+    spock = cv2.resize(spock, (200, 200))
+
+    return {"Rock": rock, "Paper": paper, "Scissors": scissors,"Lizard": lizard,"Spock": spock}
